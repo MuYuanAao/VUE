478e420b74619a1de0fa55c385690659487400f1
Yuan <931645243@qq.com>
2021-07-29 10:17:44 +0800

    7.28 todolist完结

---
+++ github_public/src/components/Search.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="jumbotron">
+        <h3 class="jumbotron-heading">Search Github Users</h3>
+        <div>
+          <input type="text" placeholder="enter the name you search" v-model="keyWord"/>&nbsp;
+          <button @click="searchUsers">Search</button>
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'Search',
+    data() {
+        return {
+            keyWord:''
+        }
+    },
+    methods: {
+        searchUsers(){
+            this.$bus.$emit('updataListData',{isLoading:true,errMsg:'',users:[],isFirst:false})
+            axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+                response =>{
+                    //请求成功后更新list的数据
+                    console.log('请求成功了')
+                    this.$bus.$emit('updataListData',{isLoading:true,errMsg:'',users:response.data.items})
+                },
+                error=>{
+                    //请求成功后更新list的数据
+                    console.log('请求失败了',error.message)
+                    this.$bus.$emit('updataListData',{isLoading:false,errMsg:error.message,users:[]})
+                }
+            )
+        }
+    },
+}
+</script>
